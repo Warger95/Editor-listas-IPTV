831db390f3891d35761735afe5f05e1731d374b2
Jorge <jorgeandresastudillo@gmail.com>
2021-08-28 22:30:36 +0200

    Creado los botones de cargar y guardar nuevo, junto con uno de información. Además se ha añadido un lbl para ver lo que carga y se ha puesto un icono

--- Editor-listas-IPTV/Form1.Designer.cs
+++ Editor-listas-IPTV/Form1.Designer.cs
@@ -29,13 +29,103 @@ namespace Editor_listas_IPTV
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.openFileDialogIPTV = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialogIPTV = new System.Windows.Forms.SaveFileDialog();
+            this.lblListaLeida = new System.Windows.Forms.Label();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.cargarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.guardarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.infoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // openFileDialogIPTV
+            // 
+            this.openFileDialogIPTV.FileName = "openFileDialog1";
+            // 
+            // lblListaLeida
+            // 
+            this.lblListaLeida.AutoSize = true;
+            this.lblListaLeida.Location = new System.Drawing.Point(0, 24);
+            this.lblListaLeida.Name = "lblListaLeida";
+            this.lblListaLeida.Size = new System.Drawing.Size(38, 15);
+            this.lblListaLeida.TabIndex = 0;
+            this.lblListaLeida.Text = "label1";
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.cargarToolStripMenuItem,
+            this.guardarToolStripMenuItem});
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(60, 20);
+            this.toolStripMenuItem1.Text = "Archivo";
+            // 
+            // cargarToolStripMenuItem
+            // 
+            this.cargarToolStripMenuItem.Name = "cargarToolStripMenuItem";
+            this.cargarToolStripMenuItem.Size = new System.Drawing.Size(116, 22);
+            this.cargarToolStripMenuItem.Text = "Cargar";
+            this.cargarToolStripMenuItem.Click += new System.EventHandler(this.CargarToolStripMenuItem_Click);
+            // 
+            // guardarToolStripMenuItem
+            // 
+            this.guardarToolStripMenuItem.Name = "guardarToolStripMenuItem";
+            this.guardarToolStripMenuItem.Size = new System.Drawing.Size(116, 22);
+            this.guardarToolStripMenuItem.Text = "Guardar";
+            this.guardarToolStripMenuItem.Click += new System.EventHandler(this.GuardarToolStripMenuItem_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItem1,
+            this.infoToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // infoToolStripMenuItem
+            // 
+            this.infoToolStripMenuItem.Name = "infoToolStripMenuItem";
+            this.infoToolStripMenuItem.Size = new System.Drawing.Size(40, 20);
+            this.infoToolStripMenuItem.Text = "Info";
+            this.infoToolStripMenuItem.Click += new System.EventHandler(this.InfoToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Controls.Add(this.lblListaLeida);
+            this.Controls.Add(this.menuStrip1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Editor de listas IPTV";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFileDialogIPTV;
+        private System.Windows.Forms.SaveFileDialog saveFileDialogIPTV;
+        private System.Windows.Forms.Label lblListaLeida;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem cargarToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem guardarToolStripMenuItem;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem infoToolStripMenuItem;
     }
 }
 
--- Editor-listas-IPTV/Form1.cs
+++ Editor-listas-IPTV/Form1.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -15,7 +16,28 @@ namespace Editor_listas_IPTV
         public Form1()
         {
             InitializeComponent();
+            openFileDialogIPTV.Filter = "IPTV list (*.m3u)|*.m3u";
         }
 
+        private void CargarToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            
+            if (openFileDialogIPTV.ShowDialog() == DialogResult.OK)
+            {
+                lblListaLeida.Text = openFileDialogIPTV.FileName;
+            }
+        }
+
+        private void GuardarToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Pendiente de implementación", "Info", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+        }
+
+        private void InfoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Versión: " + Assembly.GetEntryAssembly().GetName().Version + Environment.NewLine + "Desarrollado por Warger95", "Info", 
+                MessageBoxButtons.OK, 
+                MessageBoxIcon.Exclamation);
+        }
     }
 }
